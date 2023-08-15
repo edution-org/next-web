e45db5488b41299b43d3e1cbac66c4f0029e5444
Petr Glaser <syreanis+1@gmail.com>
2023-08-15 22:49:45 +0200

    fix(expo): remove expo

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -12,7 +12,6 @@ const config = {
   importOrder: [
     "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
     "^(next/(.*)$)|^(next$)",
-    "^(expo(.*)$)|^(expo$)",
     "<THIRD_PARTY_MODULES>",
     "",
     "^@acme/(.*)$",
