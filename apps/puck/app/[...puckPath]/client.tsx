c97c289995269df839f36d31a3bee9e1f7a723d9
Petr Glaser <syreanis+1@gmail.com>
2023-09-10 23:45:23 +0200

    feat(editor): add view page btn + headline plugin

--- apps/puck/app/[...puckPath]/client.tsx
+++ apps/puck/app/[...puckPath]/client.tsx
@@ -2,39 +2,47 @@
 
 import { useState } from "react"
 import type { Data } from "@measured/puck"
-import { Puck, Render } from "@measured/puck"
-import { Spoiler } from "components/Spoiler"
+import { Button, Puck, Render } from "@measured/puck"
+import headingAnalyzer from "@measured/puck-plugin-heading-analyzer"
 
 import config from "../../puck.config"
 
 const isBrowser = typeof window !== "undefined"
 
 export function Client({ path, dataBE, isEdit }: { path: string; dataBE: Data; isEdit: boolean }) {
-    const key = `edution-demo:${path}`
+  const key = `edution-demo:${path}`
 
-    const [data] = useState<Data>(() => {
-        if (isBrowser) {
-            const dataStr = localStorage.getItem(key)
+  const [data] = useState<Data>(() => {
+    if (isBrowser) {
+      const dataStr = localStorage.getItem(key)
 
-            if (dataStr) {
-                return JSON.parse(dataStr)
-            }
+      if (dataStr) {
+        return JSON.parse(dataStr)
+      }
 
-            return undefined
-        }
-    })
-
-    if (isEdit) {
-        return (
-            <Puck
-                config={config}
-                data={data}
-                onPublish={async (data: Data) => {
-                    localStorage.setItem(key, JSON.stringify(data))
-                }}
-            />
-        )
+      return undefined
     }
+  })
 
-    return <Render config={config} data={data} />
+  if (isEdit) {
+    return (
+      <Puck
+        config={config}
+        data={data}
+        onPublish={async (data: Data) => {
+          localStorage.setItem(key, JSON.stringify(data))
+        }}
+        plugins={[headingAnalyzer]}
+        renderHeaderActions={() => (
+          <>
+            <Button href={path} newTab variant='secondary'>
+              View page
+            </Button>
+          </>
+        )}
+      />
+    )
+  }
+
+  return <Render config={config} data={data} />
 }
